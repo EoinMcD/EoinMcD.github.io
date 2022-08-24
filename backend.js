3426241fbcf3e2344823f27adc0de221a1c17853
EoinMcD <48068135+EoinMcD@users.noreply.github.com>
2022-08-24 12:33:19 +0100

    rework

--- backend.js
+++ backend.js
@@ -14,34 +14,3 @@ function addEventListeners(imageNodes){
         });
     }
 }
-
-let femaleToggle = false;
-$("#female").click(function() {
-    if(!femaleToggle){
-        $(".portraits").filter(".female").css("backgroundColor", "rgba(255, 0, 0, 0.5)");
-        femaleToggle = true;
-
-        $('#female').css("backgroundColor", "red");
-    }else{
-        $(".portraits").filter(".female").css("backgroundColor", "");
-        femaleToggle = false;
-
-        $('#female').css("backgroundColor", "rgba(255, 255, 255, 0.2)");
-    }
-});
-
-let maleToggle = false;
-$("#male").click(function() {
-    if(!maleToggle){
-        $(".portraits").filter(".male").css("backgroundColor", "rgba(255, 0, 0, 0.5)");
-        maleToggle = true;
-
-        $('#male').css("backgroundColor", "red");
-    }else{
-        $(".portraits").filter(".male").css("backgroundColor", "");
-        maleToggle = false;
-
-        $('#male').css("backgroundColor", "rgba(255, 255, 255, 0.2)");
-
-    }
-});
